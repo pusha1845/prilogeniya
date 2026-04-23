24af49b5d0651ac4e9cd4966eccc28acb30cb327
Павел Молев <onvon5404@gmail.com>
2026-04-23 19:12:39 +0300

    lab2

---
+++ lab2/src/sasha/pkg5a/Reclntegral.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sasha.pkg5a;
+
+/**
+ *
+ * @author Александра
+ */
+public class Reclntegral {
+    private double upperLim;
+    
+    
+}
